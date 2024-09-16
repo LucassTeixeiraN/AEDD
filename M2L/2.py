e6c0a63a5fab4d0a85e25760d4c9a36e2e341ef5
João_Santos <164881489+jptrava@users.noreply.github.com>
2024-09-16 13:50:44 -0300

    Create 2.py

---
+++ M2L/2.py
@@ -0,0 +1,11 @@
+elem=10
+array=[9,8,7,6,5,4,3,10]
+
+def find_element(array , n, elem):
+    for i in range (n-1): # rodará até n
+        if array[i]==elem:# rodará até array i = elem 
+            return True
+        return False
+    
+# equação = 2n
+# O(n)
