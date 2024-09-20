470408b5f63874f1ae4a8140f401ca7e55f6d115
Andrey Rodrigues <134998417+AndreyRodr@users.noreply.github.com>
2024-09-19 21:12:08 -0300

    Update 2.py

--- M2L/2.py
+++ M2L/2.py
@@ -1,11 +1,20 @@
-elem=10
-array=[9,8,7,6,5,4,3,10]
+'''O método de lista reverse inverte os elementos da lista. Defina uma função chamada
+reverse que inverte os elementos no argumento de lista (sem usar o método reverse).
+Tente tornar essa função a mais eficiente possível e indique sua complexidade
+computacional usando a notação big-O.'''
 
-def find_element(array , n, elem):
-    for i in range (n): # rodará até n
-        if array[i]==elem:# rodará até array i = elem 
-            return True
-        return False
-    
-# equação = 2n
-# O(n)
+
+array = [9, 8, 7, 6, 5, 4, 3, 10]
+n = len(array)
+def reverse(array, n):
+    start = 0               # COMPLEXIDADE não tem pior ou melhor caso: executa igual
+    end = n - 1
+    while start < end:       # Quantas trocas aconteceram?
+        aux = array[start]        # a função itera sobre a metade da lista, invertendo o inicio e o fim, ou seja, a cada iteração o inicio e fim são trocados, portanto n/2
+        array[start] = array[end]       # Logo, O(n)
+        array[end] = aux
+        start += 1
+        end -= 1
+    print(array)
+
+reverse(array, n)  
