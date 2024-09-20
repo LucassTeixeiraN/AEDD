1044b96bf2b807bd504729fe3e2f6a60ef3baf2b
Andrey Rodrigues <134998417+AndreyRodr@users.noreply.github.com>
2024-09-19 21:55:02 -0300

    Update 11.py

--- M2L/11.py
+++ M2L/11.py
@@ -3,6 +3,13 @@ s = 0
 for i=0 to n-2:
     for j=1 to 2*N:
         s = s+1'''
+
+s = 0
+n = 5
+N = 10
+for i in range(n-2):
+    for j in range(1, 2*N+1):
+        s += 1
         
 # O primeiro laço executa n - 1 vezes (pois vai de 0 a n-2)
 # O segundo laço executa 2N vezes para cada iteração do primeiro laço
