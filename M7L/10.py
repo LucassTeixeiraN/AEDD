3ba146394d2357ed406f8419a8c9f8e2e085b925
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-12-04 16:33:59 -0300

    ex 2 e 10

--- M7L/10.py
+++ M7L/10.py
@@ -1,53 +1,143 @@
-class Grafo:
-    def __init__(self):
-        self.arestas = {}
-        self.vertices = set()
+class GraphAM:
+    __n = 0
+    __g = [[0 for column in range(10)] for row in range(10)]
+    __listofVertex = []
 
-    def adicionar_aresta(self, origem, destino, peso):
-        if origem not in self.arestas:
-            self.arestas[origem] = []
-        self.arestas[origem].append((destino, peso))
-        self.vertices.add(origem)
-        self.vertices.add(destino)
+    def __init__(self, vertex):
+        self.__listofVertex.append(vertex)
+        self.__n = len(self.__listofVertex)
+        for source in range(0, self.__n):
+            for destination in range(0, self.__n):
+                self.__g[source][destination] = 0
 
-    def dijkstra(self, inicio):
-        distancias = {vertice: float('inf') for vertice in self.vertices}
-        distancias[inicio] = 0
-        visitados = set()
-
-        while len(visitados) < len(self.vertices):
-            vertice_atual = None
-            for vertice in self.vertices:
-                if vertice not in visitados:
-                    if vertice_atual is None or distancias[vertice] < distancias[vertice_atual]:
-                        vertice_atual = vertice
-
-            if distancias[vertice_atual] == float('inf'):
-                break
-
-            visitados.add(vertice_atual)
-            for vizinho, peso in self.arestas.get(vertice_atual, []):
-                nova_distancia = distancias[vertice_atual] + peso
-                if nova_distancia < distancias[vizinho]:
-                    distancias[vizinho] = nova_distancia
+    def add_vertex(self, source, destination):
+        indexS = 0
+        indexD = 0
+        if source in self.__listofVertex:
+            indexS=self.__listofVertex.index(source)
+        else:
+            print(f"Vertex {source} not present in Graph, adding it automatically")
+            self.__listofVertex.append(source)
+            indexS=self.__listofVertex.index(source)
+            self.__n = self.__n + 1
         
-        return distancias
+        if destination in self.__listofVertex:
+            indexD = self.__listofVertex.index(destination)
+        else:
+            print(f"Vertex {destination} not present in Graph, adding it automatically")
+            self.__listofVertex.append(destination)
+            indexD=self.__listofVertex.index(destination)
+            self.__n = self.__n + 1
 
-def main():
-    grafo = Grafo()
-    arestas = [
-        ('A', 'B', 5), ('A', 'C', 3), ('A', 'D', 2),
-        ('B', 'C', 2), ('B', 'G', 1), ('C', 'E', 7),
-        ('C', 'F', 2), ('D', 'C', 7), ('D', 'F', 6),
-        ('E', 'G', 1), ('F', 'E', 1)
-    ]
+        if (indexS >= self.__n) or (indexD >= self.__n):
+            print("One of the vertex doesn't exists!")
 
-    for origem, destino, peso in arestas:
-        grafo.adicionar_aresta(origem, destino, peso)
+        if self.__n > 1:
+            for i in range(0, self.__n):
+                self.__g[i][self.__n-1] = 0
+                self.__g[self.__n-1][i] = 0
+        
+    def add_edge(self,source,destination, weight):
+        indexS=0
+        indexD=0
+        if source in self.__listofVertex:
+            indexS=self.__listofVertex.index(source)
+        else:
+            print("Cannot be included in the graph, Add the vertex {O}".format(source))
+
+        if destination in self.__listofVertex:
+            indexD=self.__listofVertex.index(destination)
+        else:
+            print(f"Cannot be included in graph, add the vertex {destination}")
+        
+        if indexS > 0 and indexS == indexD:
+            print("Same Source and Destination")
+        else:
+            self.__g[indexS][indexD] = weight
 
 
-    distancias_b = grafo.dijkstra('B')
-    print("Caminhos de custo mínimo a partir de B:")
-    for vertice, distancia in distancias_b.items():
-        print(f"B -> {vertice}: {distancia}")
-main()
+    def removeVertex(self, location):
+        indexL=0
+        if location in self.__listofVertex:
+            indexL = self.__listofVertex.index(location)
+            while indexL < self.__n :
+                for i in range(0, self.__n):
+                    self.__g[i][indexL] = self.__g[i][indexL + 1]
+
+                for i in range(0, self.__n):
+                    self.__g[indexL][i] = self.__g[indexL + 1][i]
+                indexL = indexL + 1
+
+            self.__n = self.__n - 1
+            print(f"Successfully removed {location} from graph,current list Of vertex are below:\n{self.__listofVertex}")
+        else:
+            print("No such vertex exist in the graph")                
+
+    def print_graph(self):
+        print("\n")
+        for i in range(len(self.__listofVertex)):
+            print("\t", self.__listofVertex[i], end = "")
+        for i in range(0, self.__n):
+            print("\n", self.__listofVertex[i], end=" ")
+            for j in range(0, self.__n):
+                print("\t", self.__g[i][j], end="")
+        print("\n")
+
+    def min_path(self, start_vertex):
+        if start_vertex not in self.__listofVertex:
+            print(f"O vértice {start_vertex} não existe no grafo.")
+            return
+
+        start_index = self.__listofVertex.index(start_vertex)
+        distances = [float('inf')] * self.__n  # Inicialmente, todas as distâncias são infinitas
+        distances[start_index] = 0  # Distância do vértice inicial para si mesmo é 0
+        visited = [False] * self.__n  # Nenhum vértice foi visitado no início
+
+        for _ in range(self.__n):
+            # Encontrar o vértice não visitado com a menor distância atual
+            min_distance = float('inf')
+            min_index = -1
+            for i in range(self.__n):
+                if not visited[i] and distances[i] < min_distance:
+                    min_distance = distances[i]
+                    min_index = i
+
+            # Marcar o vértice como visitado
+            visited[min_index] = True
+
+            # Atualizar as distâncias dos vértices adjacentes
+            for j in range(self.__n):
+                if self.__g[min_index][j] > 0 and not visited[j]:  # Existe uma aresta
+                    new_distance = distances[min_index] + self.__g[min_index][j]
+                    if new_distance < distances[j]:
+                        distances[j] = new_distance
+
+        # Imprimir os resultados
+        print(f"Menores distâncias a partir do vértice {start_vertex}:")
+        for i in range(self.__n):
+            if distances[i] == float('inf'):
+                print(f"Para {self.__listofVertex[i]}: Inalcançável")
+            else:
+                print(f"Para {self.__listofVertex[i]}: {distances[i]}")
+
+
+g = GraphAM("A")
+g.add_vertex("B", "C")
+g.add_vertex("D", "E")
+g.add_vertex("F", "G")
+g.add_edge("A", "B", 5)
+g.add_edge("A", "C", 3)
+g.add_edge("B", "G", 1)
+g.add_edge("B", "E", 3)
+g.add_edge("B", "C", 2)
+g.add_edge("C", "E", 7)
+g.add_edge("C", "D", 7)
+g.add_edge("D", "A", 2)
+g.add_edge("D", "F", 6)
+g.add_edge("E", "D", 2)
+g.add_edge("E", "F", 1)
+g.add_edge("G", "E", 1)
+
+g.print_graph()
+
+g.min_path("A")
--- M7L/2.py
+++ M7L/2.py
@@ -1,48 +1,124 @@
-class Grafo:
-    def __init__(self):
-        self.matriz_adj = []
-        self.vertices = {}
+'''2. Crie um programa capaz de:
+a. Instanciar um novo grafo.
+b. Adicionar um vértice ao grafo.
+c. Conectar dois vértices.
+d. Imprimir o grafo em forma de Matriz de Adjacências.
+e. Obter todos os nós adjacentes (vizinhos) a um nó do grafo (usando a MA).'''
 
-    def adicionar_vertice(self, vertice):
-        if vertice not in self.vertices:
-            self.vertices[vertice] = len(self.vertices)
+class GraphAM:
+    __n = 0
+    __g = [[0 for column in range(10)] for row in range(10)]
+    __listofVertex = []
+
+    def __init__(self, vertex):
+        self.__listofVertex.append(vertex)
+        self.__n = len(self.__listofVertex)
+        for source in range(0, self.__n):
+            for destination in range(0, self.__n):
+                self.__g[source][destination] = 0
+
+    def add_vertex(self, source, destination):
+        indexS = 0
+        indexD = 0
+        if source in self.__listofVertex:
+            indexS=self.__listofVertex.index(source)
+        else:
+            print(f"Vertex {source} not present in Graph, adding it automatically")
+            self.__listofVertex.append(source)
+            indexS=self.__listofVertex.index(source)
+            self.__n = self.__n + 1
         
-            for linha in self.matriz_adj:
-                linha.append(0)
-            self.matriz_adj.append([0] * (len(self.vertices)))
+        if destination in self.__listofVertex:
+            indexD = self.__listofVertex.index(destination)
         else:
-            print(f"Vértice {vertice} já existe no grafo.")
+            print(f"Vertex {destination} not present in Graph, adding it automatically")
+            self.__listofVertex.append(destination)
+            indexD=self.__listofVertex.index(destination)
+            self.__n = self.__n + 1
 
-    def conectar_vertices(self, vertice1, vertice2):
-        if vertice1 in self.vertices and vertice2 in self.vertices:
-            i = self.vertices[vertice1]
-            j = self.vertices[vertice2]
-            self.matriz_adj[i][j] = 1
+        if (indexS >= self.__n) or (indexD >= self.__n):
+            print("One of the vertex doesn't exists!")
+
+        if self.__n > 1:
+            for i in range(0, self.__n):
+                self.__g[i][self.__n-1] = 0
+                self.__g[self.__n-1][i] = 0
+        
+    def add_edge(self,source,destination):
+        indexS=0
+        indexD=0
+        if source in self.__listofVertex:
+            indexS=self.__listofVertex.index(source)
         else:
-            print(f"Um ou ambos os vértices {vertice1}, {vertice2} não existem no grafo.")
+            print("Cannot be included in the graph, Add the vertex {O}".format(source))
 
-    def imprimir_matriz_adj(self):
-        print("Matriz de Adjacência:")
-        for linha in self.matriz_adj:
-            print(" ".join(map(str, linha)))
-
-    def obter_vizinhos(self, vertice):
-        if vertice in self.vertices:
-            i = self.vertices[vertice]
-            vizinhos = [v for v, j in self.vertices.items() if self.matriz_adj[i][j] == 1]
-            return vizinhos
+        if destination in self.__listofVertex:
+            indexD=self.__listofVertex.index(destination)
         else:
-            print(f"Vértice {vertice} não existe no grafo.")
-            return []
-def main():
-    grafo = Grafo()
-    grafo.adicionar_vertice('A')
-    grafo.adicionar_vertice('B')
-    grafo.adicionar_vertice('C')
-    grafo.conectar_vertices('A', 'B')
-    grafo.conectar_vertices('B', 'C')
-    grafo.imprimir_matriz_adj()
-    print(f"Vizinhos do nó 'A': {grafo.obter_vizinhos('A')}")
-    print(f"Vizinhos do nó 'B': {grafo.obter_vizinhos('B')}")
-    
-main()
+            print(f"Cannot be included in graph, add the vertex {destination}")
+        
+        if indexS > 0 and indexS == indexD:
+            print("Same Source and Destination")
+        else:
+            self.__g[indexS][indexD] = 1
+            self.__g[indexD][indexS] = 1
+
+    def removeVertex(self, location):
+        indexL=0
+        if location in self.__listofVertex:
+            indexL = self.__listofVertex.index(location)
+            while indexL < self.__n :
+                for i in range(0, self.__n):
+                    self.__g[i][indexL] = self.__g[i][indexL + 1]
+
+                for i in range(0, self.__n):
+                    self.__g[indexL][i] = self.__g[indexL + 1][i]
+                indexL = indexL + 1
+
+            self.__n = self.__n - 1
+            print(f"Successfully removed {location} from graph,current list Of vertex are below:\n{self.__listofVertex}")
+        else:
+            print("No such vertex exist in the graph")                
+
+    def print_graph(self):
+        print("\n")
+        for i in range(len(self.__listofVertex)):
+            print("\t", self.__listofVertex[i], end = "")
+        for i in range(0, self.__n):
+            print("\n", self.__listofVertex[i], end=" ")
+            for j in range(0, self.__n):
+                print("\t", self.__g[i][j], end="")
+        print("\n")
+
+    def print_adjacent(self, node):
+
+        if node not in self.__listofVertex:
+            print(f"O nó {node} não existe no grafo.")
+            return
+
+        index = self.__listofVertex.index(node)
+        adjacent_nodes = []
+
+        # Iterar sobre a linha correspondente na matriz de adjacência
+        for i in range(self.__n):
+            if self.__g[index][i] == 1:  # Verificar se há uma conexão
+                adjacent_nodes.append(self.__listofVertex[i])
+
+        if adjacent_nodes:
+            print(f"Nós adjacentes ao nó {node}: {', '.join(adjacent_nodes)}")
+        else:
+            print(f"O nó {node} não tem nós adjacentes.")
+
+
+g = GraphAM("A")
+g.add_vertex("B", "C")
+g.add_edge("A", "B")
+g.add_edge("B", "C")
+g.add_edge("A", "C")
+
+g.print_graph()
+
+# Imprimir nós adjacentes
+g.print_adjacent("A")  # Deve listar "B, C"
+g.print_adjacent("B")  # Deve listar "A, C"
+g.print_adjacent("D")  # Deve informar que o nó não existe
